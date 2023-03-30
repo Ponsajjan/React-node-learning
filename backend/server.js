de31815cb1c3c51ae25c2198296a15649bf1fac1
Ponsajjan <sajjan@mavencart.com>
2023-03-30 12:03:16 +0530

    upload to git

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+// app.use(api end point, file path)
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(port, () => console.log(`server started on port ${port}`))
